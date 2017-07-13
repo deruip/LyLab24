2423f7d6a1726bfc3f6d69d4a1e3010b0c66471b
deruip <aronly1997@hotmail.com>
2017-07-13 11:47:48 -0400

    Added SuperHero subclass

---
+++ COMP123-S2017-Lesson9B/SuperHero.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Aron Ly
+ * Date: July 13, 2017
+ * Description: This is the SuperHero class which inherits from the SuperHuman class
+ * Version: 0.8 - implemented IHasKarma interface
+ */
+namespace COMP123_S2017_Lesson9B
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class SuperHero : SuperHuman, IHasKarma
+    {
+        //private instance variables
+        private int _karma;
+        //public properties
+        public int Karma
+        {
+            get
+            {
+                return this._karma;
+            }
+            set
+            {
+                this._karma = value;
+            }
+        }
+        //constructors
+        /// <summary>
+        /// This is the main constructor for the SuperHero class
+        /// It takes two arguments - name (string) - karma (int)
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="karma"></param>
+        public SuperHero(string name, int karma) 
+            :base (name)
+            {
+            this.Karma = karma;
+            }
+        //private methods
+
+        //public methods
+
+    }
+}
--- COMP123-S2017-Lesson9B/SuperVillain.cs
+++ COMP123-S2017-Lesson9B/SuperVillain.cs
@@ -7,18 +7,38 @@ using System.Threading.Tasks;
 /*
  * Name: Aron Ly
  * Date: July 13, 2017
- * Description: This is the SuperVillain subclass that extends the SuperHuman class
+ * Description: This is the SuperVillain that inherits from the SuperHuman class
  * Version: 0.1 - Class created
  */
 namespace COMP123_S2017_Lesson9B
 {
     class SuperVillain : SuperHuman, IHasMalice
     {
-        public SuperVillain(string name)
+        //constructor
+        /// <summary>
+        /// This is the main constructor for the SuperVillain class
+        /// It takes two arguments - name (string) - malice (int)
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="malice"></param>
+        public SuperVillain(string name, int malice)
             : base(name)
         {
-
+            this.Malice = malice;
         }
-        
+        //private instance variables
+        private int _malice;
+        //public properties
+        public int Malice
+        {
+            get
+            {
+                return this._malice;
+            }
+            set
+            {
+                this._malice = value;
+            }
+        }
     }
 }
